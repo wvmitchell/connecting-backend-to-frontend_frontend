e7c2f3b133385b79ef57f9e3654a70e9a5b2a94c
Will Mitchell <wvmitchell@gmail.com>
2018-12-10 10:33:46 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pets: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL)
+    const pets = await response.json()
+    this.setState({ pets })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
